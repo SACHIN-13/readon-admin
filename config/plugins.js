7350ce71f86b6fab0b7d96a8132ae1dcf7d70a09
Sachin Agarwal <sachin13agarwal@gmail.com>
2021-08-09 17:01:32 +0530

    configure email

--- config/plugins.js
+++ config/plugins.js
@@ -10,5 +10,17 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    email: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID),
+        secret: env('AWS_SECRET_ACCESS_KEY', process.env.AWS_ACCESS_SECRET),
+        amazon: `https://email.${env('AWS_REGION', process.env.AWS_REGION)}.amazonaws.com`
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM', 'sachin13agarwal@gmail.com'),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'sachin13agarwal@gmail.com')
+      }
+    },
   });
    
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', process.env.URL),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9f4ffc3715151caaf53c768e2149b42a'),
