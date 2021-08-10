37304735182445c4ad7e90fd08ee1f7b474f1fd6
Sachin Agarwal <sachin13agarwal@gmail.com>
2021-08-10 23:23:51 +0530

    alter env keys

--- config/plugins.js
+++ config/plugins.js
@@ -2,20 +2,20 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID),
-        secretAccessKey: env('AWS_ACCESS_SECRET', process.env.AWS_ACCESS_SECRET),
-        region: env('AWS_REGION', process.env.AWS_REGION),
+        accessKeyId: env('AWS_ACCESS_KEY_ID', process.env.XAWS_ACCESS_KEY_ID),
+        secretAccessKey: env('AWS_ACCESS_SECRET', process.env.XAWS_ACCESS_SECRET),
+        region: env('AWS_REGION', process.env.XAWS_REGION),
         params: {
-          Bucket: env('AWS_BUCKET_NAME', process.env.AWS_BUCKET_NAME),
+          Bucket: env('AWS_BUCKET_NAME', process.env.XAWS_BUCKET_NAME),
         },
       },
     },
     email: {
       provider: 'amazon-ses',
       providerOptions: {
-        key: env('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID),
-        secret: env('AWS_SECRET_ACCESS_KEY', process.env.AWS_ACCESS_SECRET),
-        amazon: `https://email.${env('AWS_REGION', process.env.AWS_REGION)}.amazonaws.com`
+        key: env('AWS_ACCESS_KEY_ID', process.env.XAWS_ACCESS_KEY_ID),
+        secret: env('AWS_SECRET_ACCESS_KEY', process.env.XAWS_ACCESS_SECRET),
+        amazon: `https://email.${env('AWS_REGION', process.env.XAWS_REGION)}.amazonaws.com`
       },
       settings: {
         defaultFrom: env('EMAIL_DEFAULT_FROM', 'sachin13agarwal@gmail.com'),
